1a49f3d81554094b6a4e60f7ed4106081162964c
Muskan Bhandari <bmuskan007@gmail.com>
2020-05-15 18:33:39 +0545

    Updates

---
+++ precache-manifest.07e1b75397c93b469dec62cac69b2b09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f10b88613769ac21da24c7bc919fb24",
+    "url": "/Monsters-roledex/index.html"
+  },
+  {
+    "revision": "7dfa07143a6b6980fc36",
+    "url": "/Monsters-roledex/static/css/main.f1f18ec4.chunk.css"
+  },
+  {
+    "revision": "14649715d74d2d84dbff",
+    "url": "/Monsters-roledex/static/js/2.a9e362ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-roledex/static/js/2.a9e362ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dfa07143a6b6980fc36",
+    "url": "/Monsters-roledex/static/js/main.45e286ab.chunk.js"
+  },
+  {
+    "revision": "5a929532ebe237bb24c9",
+    "url": "/Monsters-roledex/static/js/runtime-main.dd7e3ed2.js"
+  }
+]);
